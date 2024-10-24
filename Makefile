2613bfcbc8ff724f7066d2f35d93efe8d0f7e543
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-10-24 17:18:47 +0530

    Github Actions: Publishing Static site - Thu Oct 24 05:18:47 PM IST 2024

---
+++ Makefile
@@ -0,0 +1,22 @@
+DATE := $(shell date +"%Y-%m-%d")
+FILE := $(DATE)-$(TOPIC)
+GITHUB_ACCESS_TOKEN := $(shell grep -oE '^BLOG_GITHUB_ACCESS_TOKEN=(.*)' ~/.gittokens | cut -d'=' -f2)
+
+serve:
+	LANG="en_US.UTF-8" \
+	     LANGUAGE="en_US.UTF-8" \
+	     LC_CTYPE="en_US.UTF-8" \
+	     LC_MONETARY="en_US.UTF-8" \
+	     LC_NUMERIC="en_US.UTF-8" \
+	     LC_ALL="en_US.UTF-8" ./bin/jekyll serve --livereload
+
+gen.file:
+	echo $(FILE)
+	cp templates/_post.markdown _posts/$(FILE).markdown
+	nvim _posts/$(FILE).markdown
+
+clear.cache:
+	rm _cache/**
+
+remote.setup:
+	gh secret set ACCESS_TOKEN -b $(GITHUB_ACCESS_TOKEN)
---
+++ scripts/publish.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Build locally and publish to gh-pages. Fucking open source and compatibilty
+#
+set -e
+
+echo "Cleanup old _site"
+BACKUP_FILE="site_$(date +%s).tar.gz"
+
+if [[ -d "_site" ]]; then
+  echo "Backup old _site"
+  mkdir -p _site
+  tar -cvzpf "${BACKUP_FILE}" _site
+  rm -rf _site
+fi
+
+echo "⚡️ Installing project dependencies..."
+cd blog/
+bundle install --binstubs
+
+
+echo "🏋️ Building website..."
+JEKYLL_ENV=production ./bin/jekyll build
+mv _site ../ && cd ../_site
+
+
+echo "☁️  Publishing website"
+git init
+git checkout -b gh-pages
+git remote add origin git@github.com:ikouchiha47/ikouchiha47.github.io.git
+
+git add .
+git commit -m "Github Actions: Publishing Static site - $(date)"
+
+echo "Build branch ready to go. Pushing to Github..."
+echo "git push origin -f gh-pages:gh-pages"
+git push origin -f gh-pages:gh-pages
+
+echo "🎉 New version deployed 🎊"
+
